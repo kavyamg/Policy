c2bdb1e26b9d42190347687d57dc5a5332906b7e
kavyamg <kavyamg96@gmail.com>
2017-09-08 22:03:21 +0530

    created tables

--- app/src/main/java/kav/com/firebase/ManagePolicy.java
+++ app/src/main/java/kav/com/firebase/ManagePolicy.java
@@ -2,12 +2,28 @@ package kav.com.firebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ManagePolicy extends AppCompatActivity {
+    TextView t1,t2,t3;
+    FirebaseDatabase firebase=FirebaseDatabase.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_policy);
+        t1= (TextView) findViewById(R.id.pname);
+        t2= (TextView) findViewById(R.id.pnum);
+        t3= (TextView) findViewById(R.id.renDate);
+
+
+    }
+
+    public void addToDB(View view) {
+
     }
 }
